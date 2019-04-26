049d7463150734d29126567359b67dcb77cf5a3d
kotlin-studio <kotlin-studio@yandex.com>
2019-04-26 23:19:54 +0300

    Step 2.3: Toolbar

--- app/src/main/java/com/github/kotlin_studio/navigation2019/MainActivity.kt
+++ app/src/main/java/com/github/kotlin_studio/navigation2019/MainActivity.kt
@@ -3,12 +3,19 @@ package com.github.kotlin_studio.navigation2019
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v7.widget.Toolbar
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +24,37 @@ class MainActivity : AppCompatActivity() {
         // получаем navController
         val host: NavHostFragment = supportFragmentManager
             .findFragmentById(R.id.navFragment) as NavHostFragment? ?: return
-        val navController = host.navController
+        navController = host.navController
 
         // включаем боковое меню
         val sideBar = findViewById<NavigationView>(R.id.nav_view)
         sideBar?.setupWithNavController(navController)
 
+        // настраиваем панель инструментов
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout = drawer_layout)
+
+        // включаем toolbar
+        val toolBar = findViewById<Toolbar>(R.id.toolbar)
+        toolBar.setupWithNavController(navController, appBarConfiguration)
+
+
+
+        //navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
+
     }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
+
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
+//
 }
+
+
 
