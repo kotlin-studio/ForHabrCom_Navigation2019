2d0c73858c1e4970153d810fc2e52db897b1ed36
kotlin-studio <kotlin-studio@yandex.com>
2019-04-26 22:12:10 +0300

    Step 2.2: Side menu

--- app/src/main/java/com/github/kotlin_studio/navigation2019/MainActivity.kt
+++ app/src/main/java/com/github/kotlin_studio/navigation2019/MainActivity.kt
@@ -2,12 +2,27 @@ package com.github.kotlin_studio.navigation2019
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // получаем navController
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navFragment) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        // включаем боковое меню
+        val sideBar = findViewById<NavigationView>(R.id.nav_view)
+        sideBar?.setupWithNavController(navController)
+
     }
 }
 
